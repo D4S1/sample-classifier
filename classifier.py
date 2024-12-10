d1091c6bea09d8e758452342d9811d94c0a7b59c
annnie73 <aniaszymik02@gmail.com>
2024-12-10 16:42:27 +0100

    add globals

--- classifier.py
+++ classifier.py
@@ -1,8 +1,16 @@
 import classifier_helper
 
 def main():
-    hg38_path = ''
-    hg_38_sequence = classifier_helper.preprocess_human_genome(hg38_path)
-    human_kmers = classifier_helper.kmer_set(hg_38_sequence)
+    ci = 4
+    T = 0.5
+    M = 8
+    k = 24
+    s = 1000
+    seed = 7921
 
-    cities_sketches = classifier_helper.preprocess_reference()
+    hg38_path = ''
+    train_path = ''
+    hg_38_sequence = classifier_helper.preprocess_human_genome(hg38_path)
+    human_kmers = classifier_helper.kmer_set(hg_38_sequence, k, seed, ci)
+
+    cities_sketches = classifier_helper.preprocess_reference(train_path, k, s, human_kmers)
